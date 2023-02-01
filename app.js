8c174af26818fd4969dfd89f48baa813dd6fa890
ilhom003 <abdulahatov03gmail.com>
2023-02-01 20:09:07 +0500

    string methods

---
+++ app.js
@@ -0,0 +1,66 @@
+// let str1 = 'wba'
+// let str2 = "wba"
+// let str3 = `wba`
+
+// let str = 'webbrain academy web';
+
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.indexOf('bb'));
+// console.log(str.lastIndexOf('bb'));
+// console.log(str.includes('aca'));
+// console.log(str.startsWith('web'));
+// console.log(str.endsWith('web'));
+// console.log(str.padEnd(22, '.'));
+// console.log(str.padStart(22, '.'));
+// console.log(str.slice(2,7));
+// console.log(str.substring(0,2));
+// console.log(str.substr(2, 2));
+// console.log(eval('2+5'));
+// console.log(str);
+// console.log(str.trim());
+// console.log(str.split(' '));
+// console.log(str.concat(' sdfs'));
+// console.log(new String('sdfsd').valueOf());
+// console.log(str.replace('web', 'Web'));
+// console.log(str.replace(/Web/ig,'Web'));
+// for(char of str){
+//     console.log(char);
+// }
+
+
+// Homework
+
+// 1
+// function check(str1, str2) {
+//     if (str1.includes(str2.length >= 3)) {
+//         console.log('mos keladi');
+//     } else if (str1.includes(str2)) {
+//         console.log('qisman mos keladi');
+//     } else {
+//         console.log('mos kelmadi');
+//     }
+// }
+// check('webbrain', 'brain')
+
+// 3
+// function truncate(str, n) {
+//     return str.substring(0, n)
+// }
+// console.log(truncate('webbrain', 5));
+
+
+// 4
+// function getCurrency(str){
+//     return str.slice(1,str.length)
+// }
+// console.log(getCurrency('$100000'));
+
+// 5
+
+// 6
+// function capitalize(str) {
+//     return str.split().toString().toUpperCase()
+// }
+// console.log(capitalize('df dfsdd dfsd dfs'));
